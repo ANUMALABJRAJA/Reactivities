51dc8c23c56ae53efd554a0ff793d625e76b31b2
ANUMALABJRAJA <abjraja9216@gmail.com>
2025-07-19 12:47:08 +0530

    Skeleton Code of API

---
+++ API/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opts =>{
+    opts.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try{
+ var context = services.GetRequiredService<AppDbContext>();
+ await context.Database.MigrateAsync();
+ await DbInitializer.SeedData(context);
+}
+catch(Exception e){
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(e, "Errror Occured during Exception");
+}
+
+
+
+app.Run();
